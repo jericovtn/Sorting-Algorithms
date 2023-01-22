5d7268212a6d352b49c760892ebb5b31dde7693a
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2023-01-23 01:20:11 +0800

    Defining the function with left and right array

--- Merge Sort.py
+++ Merge Sort.py
@@ -3,7 +3,13 @@
 # Merge Sort
 # January 27, 2023
 
+def mergeSort(arr):
+    if len(arr) > 1:
+        leftArray = arr[:len(array)//2]
+        rightArray = arr[len(array)//2:]
 
+        mergeSort(leftArray)
+        mergeSort(rightArray    )
 
 
 array = [40, 10, 55, 96, 16, 93, 56, 4, 98, 69]
