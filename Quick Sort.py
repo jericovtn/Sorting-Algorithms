d7ca319c60c9466c36bae440cdf961f2ae545042
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2023-01-23 01:41:07 +0800

    Added quickSort definition

--- Quick Sort.py
+++ Quick Sort.py
@@ -3,6 +3,11 @@
 # Quick Sort
 # January 27, 2023
 
+def quickSort(array, left, right):
+    if left < right:
+        partitionPosition = partition(array, left, right)
+
+def partition(array, left, right):
 
 
 array = [40, 10, 55, 96, 16, 93, 56, 4, 98, 69]
