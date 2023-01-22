bef039dc35de5b061d5c31b478199fa5fe27d60c
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2023-01-23 00:44:13 +0800

    Added: print to see after every iteration

--- Selection Sort.py
+++ Selection Sort.py
@@ -14,6 +14,8 @@ def selectionSort(array):
         array[i] = array[minPosition]
         array[minPosition] = temporary
 
+        print(array)
+
 array = [40, 10, 55, 96, 16, 93, 56, 4, 98, 69]
 
 selectionSort(array)
