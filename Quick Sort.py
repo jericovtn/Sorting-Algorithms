ab2ada086b2c1b2bb13c951ea548ef47757e106c
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2023-01-23 01:46:49 +0800

    Added: Indexes

--- Quick Sort.py
+++ Quick Sort.py
@@ -10,6 +10,12 @@ def quickSort(array, left, right):
         quickSort(array, partitionPosition + 1, right)
 
 def partition(array, left, right):
+    # Indexes
+    i = left # defines the left point to area to sort
+    j = right - 1 # defines the right point of the pivot
+    pivot = array[right] # array at the index right
+
+
 
 
 array = [40, 10, 55, 96, 16, 93, 56, 4, 98, 69]
