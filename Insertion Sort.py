5f7b77f6235e7716d5d8ec7023d73e9cd96d41d0
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2023-01-23 01:08:25 +0800

    Added: Insertion Sort Method with print

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -3,6 +3,14 @@
 # Insertion Sort
 # January 27, 2023
 
-
-
+def insertionSort(array):
+    for i in range(1, len(array)):
+        j = i
+        while array[j - 1] > array[j] and j > 0:
+            array[j - 1], array[j] = array[j], array[j - 1]
+            j -= 1
+        
 array = [40, 10, 55, 96, 16, 93, 56, 4, 98, 69]
+
+insertionSort(array)
+print(array)
