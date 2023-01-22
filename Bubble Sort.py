10808b44212ca928e2d122e9d5d455fef32a44fc
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2023-01-23 00:53:10 +0800

    Added: Bubble Sort Method with print

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -3,4 +3,16 @@
 # Bubble Sort
 # January 27, 2023
 
-numbers = [40, 10, 55, 96, 16, 93, 56, 4, 98, 69]
+def bubbleSort(array):
+    for i in range(len(array) -1, 0, -1):
+        for j in range(i):
+            if array[j] > array[j + 1]:
+                temporary = array[j]
+                array[j] = array[j + 1]
+                array [j + 1] = temporary
+
+
+array = [40, 10, 55, 96, 16, 93, 56, 4, 98, 69]
+
+bubbleSort(array)
+print(array)
